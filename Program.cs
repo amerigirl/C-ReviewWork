2624c1b5f42bb0f3bbe4900ec3b6b2b8b5163613
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-18 15:52:02 -0800

    initial commit, working in C#

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C# Programmers!");
+Console.WriteLine("Hello, C# Programmers!");
+
+
+Console.Write("First");
+Console.Write("Second");
